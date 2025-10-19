d9b35b3b45449483bc19aeef3d45450366f3f257
Kmberry1989 <kylematthewberry@gmail.com>
2025-10-18 22:17:36 -0400

    poo

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1760839581|44622511';
+const CACHE_VERSION = '1760840180|644371151';
 /** @type {string} */
 const CACHE_PREFIX = 'Meaner Matcher-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
